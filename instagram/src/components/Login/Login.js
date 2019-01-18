2b8178b756c4dc7a97f996bd1554241602f96167
joshua-disney <joshua.a.disney@gmail.com>
2019-01-17 19:05:57 -0700

    Took autoComplete off of Login input fields.

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -77,6 +77,7 @@ class Login extends React.Component {
                 name='username'
                 placeholder='username'
                 onChange={this.handleInputChange}
+                autoComplete='off'
               />
               <LoginInput
                 value={this.state.password}
@@ -84,6 +85,7 @@ class Login extends React.Component {
                 name='password'
                 placeholder='password'
                 onChange={this.handleInputChange}
+                autoComplete='off'
               />
             </InputSection>
             <LoginButton onClick={this.handleChanges}>Login</LoginButton>
