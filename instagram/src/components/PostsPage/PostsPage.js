c38cd85fe774bc352dd70a6a48452dd7c085b643
joshua-disney <joshua.a.disney@gmail.com>
2019-07-31 19:51:40 -0700

    No idea what I changed.  Let's find out.

--- instagram/src/components/PostsPage/PostsPage.js
+++ instagram/src/components/PostsPage/PostsPage.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import dummyData from '../../dummy-data';
-import SearchBar from '../SearchBar/SearchBar';
-import PostContainer from '../PostContainer/PostContainer';
-
+import dummyData from "../../dummy-data";
+import SearchBar from "../SearchBar/SearchBar";
+import PostContainer from "../PostContainer/PostContainer";
 
 const PostsContainer = styled.div`
   max-width: 650px;
@@ -15,48 +14,46 @@ class PostsPage extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      dummyData : [],
-      posts: ''
-    }
+      dummyData: [],
+      posts: ""
+    };
   }
 
   componentDidMount() {
-    this.setState({dummyData});
-  };
+    this.setState({ dummyData });
+  }
 
   filterPosts = () => {
     if (this.state.posts) {
       return this.state.dummyData.filter(posts => {
-        return posts.username.includes(this.state.posts)
-      })
-    } return this.state.dummyData
+        return posts.username.includes(this.state.posts);
+      });
+    }
+    return this.state.dummyData;
   };
 
   handleChanges = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
-  logOut = (event) => {
-    localStorage.removeItem('user');
+  logOut = event => {
+    localStorage.removeItem("user");
     window.location.reload();
   };
 
   render() {
     const filteredPosts = this.filterPosts();
     return (
-      <div className='PostsPage'>
+      <div className="PostsPage">
         <SearchBar
           posts={this.state.posts}
           handleChanges={this.handleChanges}
         />
         <PostsContainer>
-          {
-            filteredPosts && filteredPosts.map((post) => {
-              return <PostContainer
-                key={post.timestamp}
-                post={post} />
-            })
-          }
+          {filteredPosts &&
+            filteredPosts.map(post => {
+              return <PostContainer key={post.timestamp} post={post} />;
+            })}
           <form onSubmit={this.logOut}>
             <button onClick={this.handleChanges}>Log Out</button>
           </form>
